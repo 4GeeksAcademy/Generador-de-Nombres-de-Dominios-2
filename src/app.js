121bcbf77a25852752450a6a87e6c79e28ed1a55
Carlos Narváez <narvaezcarlos88@gmail.com>
2026-02-23 19:43:07 +0000

    updates

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-import "bootstrap";
-import "./style.css";
+//arrays
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon', 'pued'];
+let exte=['.com', '.net', '.us', '.io', '.es'];
 
+let arrayWords=[];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let l = 0; l < exte.length; l++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let k = 0; k < noun.length; k++) {      
+        arrayWords=pronoun[i]+adj[j]+noun[k]+exte[l];
+        console.log(arrayWords);
+      }
+    }
+  }
+}
